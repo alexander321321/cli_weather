fba253474e1e3894728fa1066b60dd8c5ad32205
alexander321321 <treesome@yandex.ru>
2022-02-03 11:50:10 +0300

    start commit

---
+++ char.mjs
@@ -0,0 +1,19 @@
+let characters = [
+  { name: "Frodo", hasRing: false },
+  { name: "Bilbo", hasRing: false }
+]
+
+function stealRing(characters, owner) {
+  characters.map(c => {
+    if (c.name === owner) {
+      c.hasRing = true
+    } else {
+      c.hasRing = false
+    }
+  })
+}
+
+module.exports = {
+  characters,
+  stealRing
+}
