e56cbcd09df16cbeb1e0b905a7794308c628493f
alexander321321 <treesome@yandex.ru>
2022-02-04 00:44:19 +0300

    errors

--- weather.js
+++ weather.js
@@ -17,6 +17,21 @@ const saveToken = async (token) => {
   }
 }
 
+const getForcast = async () => {
+  try {
+    const weather = await getWeather(process.env.CITY);
+    console.log(weather); // Красивый вывод погоды
+  } catch (e) {
+    if (e?.response?.status == 404) {
+      printError('Неверно указан город');
+    } else if (e?.response?.status == 401) {
+      printError('Неверно указан токен');
+    } else {
+      printError(e.message);
+    }
+  }
+}
+
 const initCLI = () => {
   const args = getArgs(process.argv)
   if (args.h) {
