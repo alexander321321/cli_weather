610928bfadc8437a69812a7d5846c4ea061fc89e
alexander321321 <treesome@yandex.ru>
2022-02-06 12:31:04 +0300

    errors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { App } from "./app";
+import { ExeptionFilter } from "./errors/exeption.filter";
 import { UserController } from './users/users.controller';
 import { LoggerService } from './logger/logger.service';
 
 async function bootstrap() {
   const logger = new LoggerService()
-  const app = new App(logger, new UserController(logger));
+  const app = new App(
+    logger,
+    new UserController(logger),
+    new ExeptionFilter(logger)
+  );
   await app.init();
 }
 
